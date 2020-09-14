0d36d7ae0150575a24e7afe012944e3ca8e855d4
Dave Cramer <davecramer@gmail.com>
2020-09-14 17:03:18 -0400

    more features and command line switches

--- src/main/java/rocks/postgres/JDBCBench.java
+++ src/main/java/rocks/postgres/JDBCBench.java
@@ -5,12 +5,14 @@
 
 package rocks.postgres;
 
+import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -18,6 +20,8 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import javax.swing.plaf.nimbus.State;
 
 @Command(name="JDBCBench", version = "0.1")
 
@@ -29,8 +33,21 @@ public class JDBCBench implements Callable <Integer> {
 
 		Protocol(String label){
 				this.label = label;
+				this.name = label;
 		}
-		public final String label;
+		private final String label;
+		private final String name;
+		public String getName() {
+			return name;
+		}
+	}
+
+	private enum InitSteps {
+		DROP,
+		CREATE_TABLES,
+		VACUUM,
+		PRIMARY_KEYS,
+		FOREIGN_KEYS
 	}
 
 	/* tpc bm b scaling rules */
@@ -48,33 +65,47 @@ public class JDBCBench implements Callable <Integer> {
 	/* we will use host, port, user, database */
 	@Deprecated
 
-
-	@Option(names={"--transactions", "-t"}, description = "Number of transactions each client runs. Default is ${DEFAULT-VALUE}", defaultValue = "10")
-	private int transactionsPerClient = 10;
-
 	@Option(names={"--clients", "-c"}, description = "Number of clients simulated, that is, number of concurrent database sessions. Default is ${DEFAULT-VALUE}", defaultValue = "1")
 	private int numClients = 10;
-
-	@Option(names={"--select", "-s"}, description = "Run built-in select-only script.", defaultValue = "false")
-	private boolean selectOnly = false;
-
-	@Option(names = {"--vacuum-all", "-v"}, description = "Vacuum all four standard tables before running the test. With neither -n nor -v, pgbench will vacuum the pgbench_tellers and pgbench_branches tables, and will truncate pgbench_history.", defaultValue = "false")
-	private boolean vacuumAll = false;
-
-	@Option(names={"--no-vacuum", "-n"}, description = "Perform no vacuuming before running the test. This option is necessary if you are running a custom test scenario that does not include the standard tables", defaultValue = "false")
-	private boolean noVacuum = false;
-
-	@Option(names={"--protocol", "-M"}, description = "", defaultValue = "SIMPLE")
-	private Protocol protocol= Protocol.SIMPLE;
 
 	@Option(names={"--fillfactor", "-F"}, description = "Create the pgbench_accounts, pgbench_tellers and pgbench_branches tables with the given fill factor. Default is ${DEFAULT-VALUE}", defaultValue = "100")
 	private int fillFactor = 100;
 
+	@Option(names = {"--drop-tables"}, description = "Drop tables before create", defaultValue = "false")
+	private boolean dropTables = false;
+
+	@Option(names = {"--big-integers"}, description = "Use bigInteger for account id .", defaultValue = "false")
+	private boolean bigIntegers = false;
+
+	@Option(names = {"--primary-keys"}, description = "Create primary key indexes on  the standard tables.", defaultValue = "false")
+	private boolean primaryKeys = false;
+
+	@Option(names = {"--foreign-keys"}, description = "Create foreign key constraints between the standard tables.", defaultValue = "false")
+	private boolean foreignKeys = false;
+
+	@Option(names = {"--truncate"}, description = "truncate tables before run", defaultValue = "false")
+	private boolean truncate = false;
+
 	@Option(names = {"--host", "-h"}, description = "Database server's host name", defaultValue = "localhost")
 	private String host = "localhost";
 
+	@Option(names={"--no-vacuum", "-n"}, description = "Perform no vacuuming before running the test. This option is necessary if you are running a custom test scenario that does not include the standard tables", defaultValue = "false")
+	private boolean noVacuum = false;
+
 	@Option(names={"--port", "-p"}, description = "Database server's port number", defaultValue = "5432")
 	private int port = 5432;
+
+	@Option(names={"--protocol", "-M"}, description = "", defaultValue = "SIMPLE")
+	private Protocol protocol= Protocol.SIMPLE;
+
+	@Option(names={"--select", "-s"}, description = "Run built-in select-only script.", defaultValue = "false")
+	private boolean selectOnly = false;
+
+	@Option(names={"--transactions", "-t"}, description = "Number of transactions each client runs. Default is ${DEFAULT-VALUE}", defaultValue = "10")
+	private int transactionsPerClient = 10;
+
+	@Option(names = {"--vacuum-all", "-v"}, description = "Vacuum all four standard tables before running the test. With neither -n nor -v, pgbench will vacuum the pgbench_tellers and pgbench_branches tables, and will truncate pgbench_history.", defaultValue = "false")
+	private boolean vacuumAll = false;
 
 	@Parameters(defaultValue = "pgbench", description = "Database Name")
 	private String dbName;
@@ -112,7 +143,7 @@ public class JDBCBench implements Callable <Integer> {
 
 	@Override
 	public Integer call() {
-		 dbUrl = createUrl(host, port, dbName);
+		dbUrl = createUrl(host, port, dbName);
 		System.out
 				.println("*********************************************************");
 		System.out
@@ -164,9 +195,15 @@ public class JDBCBench implements Callable <Integer> {
 		try {
 			if (init) {
 				System.out.print("Initializing dataset...");
-				createDatabase(con);
+				createDatabase(con, primaryKeys, foreignKeys, bigIntegers );
 				System.out.println("done.\n");
 			}
+
+			if ( vacuumAll ) {
+				System.out.println("Vacuuming tables ");
+				vacuum( con );
+			}
+
 			System.out.println("* Starting Benchmark Run *");
 			MemoryWatcher = new MemoryWatcherThread();
 			MemoryWatcher.start();
@@ -242,49 +279,153 @@ public class JDBCBench implements Callable <Integer> {
 		failedTransactions++;
 	}
 
+	void runInitSteps(List <InitSteps> initSteps ) throws SQLException {
+
+	}
 	/*
 	 * createDatabase() - Creates and Initializes a scaled database.
 	 */
 
-	void createDatabase(Connection conn) {
+	void dropTables( Connection con ) throws  SQLException {
+		try (Statement stmt = con.createStatement()) {
+			stmt.execute("drop table if exists "
+					+ "pgbench_accounts, "
+					+ "pgbench_branches, "
+					+ "pgbench_history, "
+					+ "pgbench_tellers");
+		}
+	}
+	void initTruncateTables(Connection con) throws SQLException
+	{
+		try ( Statement stmt = con.createStatement() ) {
+			stmt.execute("truncate table "
+					+ "pgbench_accounts, "
+					+ "pgbench_branches, "
+					+ "pgbench_history, "
+					+ "pgbench_tellers");
+		}
+	}
+	void initCreatePKeys(Connection con) throws SQLException
+	{
+		final String[] DDL_INDEXES = {
+				"alter table pgbench_branches add primary key (bid)",
+				"alter table pgbench_tellers add primary key (tid)",
+				"alter table pgbench_accounts add primary key (aid)"
+		};
+
+		try ( Statement stmt = con.createStatement() ) {
+			for ( int i=0; i< DDL_INDEXES.length; i++ ) {
+				stmt.execute(DDL_INDEXES[i]);
+			}
+		}
+	}
+
+	void initCreateFKeys(Connection con) throws SQLException
+	{
+		final String[] DDL_KEYS = {
+				"alter table pgbench_tellers add constraint pgbench_tellers_bid_fkey foreign key (bid) references pgbench_branches",
+				"alter table pgbench_accounts add constraint pgbench_accounts_bid_fkey foreign key (bid) references pgbench_branches",
+				"alter table pgbench_history add constraint pgbench_history_bid_fkey foreign key (bid) references pgbench_branches",
+				"alter table pgbench_history add constraint pgbench_history_tid_fkey foreign key (tid) references pgbench_tellers",
+				"alter table pgbench_history add constraint pgbench_history_aid_fkey foreign key (aid) references pgbench_accounts"		};
+
+		try ( Statement stmt = con.createStatement() ) {
+			for ( int i=0; i< DDL_KEYS.length; i++ ) {
+				stmt.execute(DDL_KEYS[i]);
+			}
+		}
+	}
+	private class PgColumn {
+		String name;
+		String type;
+		PgColumn (String name, String type) {
+			this.name = name;
+			this.type = type;
+		}
+	}
+	private class DDLInfo {
+		String tableName;
+		List <PgColumn> columnList;
+		DDLInfo( String tableName ) {
+			this.tableName = tableName;
+			columnList = new ArrayList<>();
+		}
+		DDLInfo addColumn(PgColumn pgColumn) {
+			columnList.add(pgColumn);
+			return this;
+		}
+		String createTable() {
+			StringBuilder sb = new StringBuilder("create table ")
+					.append(tableName)
+					.append('(');
+			for(int i=0; i< columnList.size();i++) {
+				PgColumn column = columnList.get(i);
+				sb.append(' ')
+				.append(column.name)
+				.append(' ')
+				.append(column.type);
+				if ( (i+1) < columnList.size() ) {
+					sb.append(',');
+				}
+
+			}
+			sb.append(')');
+			return sb.toString();
+		}
+	}
+
+	void createDatabase(Connection conn, boolean foreignKeys, boolean primaryKeys, boolean bigIntegers) {
+
+		String query;
+		final List <DDLInfo>  tableList = new ArrayList<DDLInfo>();
+		tableList.add( new DDLInfo("pgbench_history").addColumn(new PgColumn("tid", "int"))
+				.addColumn(new PgColumn("bid", "int"))
+				.addColumn(new PgColumn("aid", bigIntegers?"bigint":"int"))
+				.addColumn(new PgColumn("delta", "int"))
+				.addColumn(new PgColumn("mtime", "timestamp"))
+				.addColumn(new PgColumn("filler", "char(22)")));
+
+		tableList.add( new DDLInfo( "pgbench_tellers")
+				.addColumn(new PgColumn("tid", "int not null"))
+				.addColumn(new PgColumn("bid", "int"))
+				.addColumn(new PgColumn("tbalance", "int"))
+				.addColumn(new PgColumn("filler", "char(84)")));
+
+		tableList.add( new DDLInfo( "pgbench_accounts")
+				.addColumn(new PgColumn("aid", bigIntegers?"bigint":"int" + " not null"))
+				.addColumn(new PgColumn("bid", "int"))
+				.addColumn(new PgColumn("abalance", "int"))
+				.addColumn(new PgColumn("filler", "char(84)")));
+
+		tableList.add( new DDLInfo( "pgbench_branches")
+				.addColumn(new PgColumn("bid", "int not null"))
+				.addColumn(new PgColumn("bbalance", "int"))
+				.addColumn(new PgColumn("filler", "char(88)")));
+
+		try {
+			if (dropTables) {
+				dropTables(conn);
+			}
+		} catch (Exception ex ) {
+			System.err.println("Error dropping tables ");
+			ex.printStackTrace(System.err);
+		}
 
 		try (Statement stmt = conn.createStatement()) {
 
-			String query = "CREATE TABLE branches (";
-			query += "bid         INT NOT NULL, PRIMARY KEY(bid), ";
-			query += "bbalance    INT,";
-			query += "filler      CHAR(88))"; /* pad to 100 bytes */
-			stmt.executeUpdate(query);
-			stmt.clearWarnings();
+			for ( DDLInfo tableDDL:tableList ) {
+				String q = tableDDL.createTable();
+				stmt.executeUpdate(q);
+				stmt.clearWarnings();
+			}
 
-			query = "CREATE TABLE tellers ( ";
-			query += "tid         INT NOT NULL, PRIMARY KEY(tid),";
-			query += "bid         INT,";
-			query += "tbalance    INT,";
-			query += "filler      CHAR(84))"; /* pad to 100 bytes */
+			if (primaryKeys) {
+				initCreatePKeys(conn);
+			}
 
-			stmt.executeUpdate(query);
-			stmt.clearWarnings();
-
-			query = "CREATE TABLE accounts ( ";
-			query += "aid         INT NOT NULL, PRIMARY KEY(aid), ";
-			query += "bid         INT, ";
-			query += "abalance    INT, ";
-			query += "filler      CHAR(84))"; /* pad to 100 bytes */
-
-			stmt.executeUpdate(query);
-			stmt.clearWarnings();
-
-			query = "CREATE TABLE history ( ";
-			query += "tid         INT, ";
-			query += "bid         INT, ";
-			query += "aid         INT, ";
-			query += "delta       INT, ";
-			query += "time        TIMESTAMP, ";
-			query += "filler      CHAR(22))"; /* pad to 50 bytes */
-
-			stmt.executeUpdate(query);
-			stmt.clearWarnings();
+			if (foreignKeys) {
+				initCreateFKeys(conn);
+			}
 
 			/*
 			 * prime database using TPC BM B scaling rules. Note that for each
@@ -293,19 +434,19 @@ public class JDBCBench implements Callable <Integer> {
 			 */
 
 			for (int i = 0; i < numBranches * scale; i++) {
-				query = "INSERT INTO branches(bid,bbalance) VALUES (" + i
+				query = "INSERT INTO pgbench_branches(bid,bbalance) VALUES (" + i
 						+ ",0)";
 				stmt.executeUpdate(query);
 				stmt.clearWarnings();
 			}
 			for (int i = 0; i < numTellers * scale; i++) {
-				query = "INSERT INTO tellers(tid,bid,tbalance) VALUES (" + i
+				query = "INSERT INTO pgbench_tellers(tid,bid,tbalance) VALUES (" + i
 						+ "," + i / numTellers + ",0)";
 				stmt.executeUpdate(query);
 				stmt.clearWarnings();
 			}
 			for (int i = 0; i < numAccounts * scale; i++) {
-				query = "INSERT INTO accounts(aid,bid,abalance) VALUES (" + i
+				query = "INSERT INTO pgbench_accounts(aid,bid,abalance) VALUES (" + i
 						+ "," + i / numAccounts + ",0)";
 				stmt.executeUpdate(query);
 				stmt.clearWarnings();
@@ -316,6 +457,12 @@ public class JDBCBench implements Callable <Integer> {
 		}
 
 	} /* end of CreateDatabase */
+
+	private void vacuum( Connection con ) throws SQLException {
+		try (Statement statement = con.createStatement() ) {
+			statement.executeQuery("vacuum");
+		}
+	}
 
 	public static int getRandomInt(int lo, int hi) {
 		int ret;
@@ -374,7 +521,7 @@ public class JDBCBench implements Callable <Integer> {
 
 				if (selectOnly) {
 					query = "SELECT abalance ";
-					query += "FROM   accounts ";
+					query += "FROM   pgbench_accounts ";
 					query += "WHERE  aid = " + aid;
 
 					try (ResultSet rs = stmt.executeQuery(query)) {
@@ -391,7 +538,7 @@ public class JDBCBench implements Callable <Integer> {
 					connection.setAutoCommit(false);
 				}
 
-				query = "UPDATE accounts ";
+				query = "UPDATE pgbench_accounts ";
 				query += "SET abalance = abalance + " + delta + " ";
 				query += "WHERE aid = " + aid;
 
@@ -399,7 +546,7 @@ public class JDBCBench implements Callable <Integer> {
 				stmt.clearWarnings();
 
 				query = "SELECT abalance ";
-				query += "FROM   accounts ";
+				query += "FROM   pgbench_accounts ";
 				query += "WHERE  aid = " + aid;
 
 				try (ResultSet rs = stmt.executeQuery(query)) {
@@ -411,19 +558,19 @@ public class JDBCBench implements Callable <Integer> {
 						aBalance = rs.getInt(1);
 					}
 
-					query = "UPDATE tellers ";
+					query = "UPDATE pgbench_tellers ";
 					query += "SET    tbalance = tbalance + " + delta + " ";
 					query += "WHERE  tid = " + tid;
 					stmt.executeUpdate(query);
 					stmt.clearWarnings();
 
-					query = "UPDATE branches ";
+					query = "UPDATE pgbench_branches ";
 					query += "SET    bbalance = bbalance + " + delta + " ";
 					query += "WHERE  bid = " + bid;
 					stmt.executeUpdate(query);
 					stmt.clearWarnings();
 
-					query = "INSERT INTO history(tid, bid, aid, delta) ";
+					query = "INSERT INTO pgbench_history(tid, bid, aid, delta) ";
 					query += "VALUES (";
 					query += tid + ",";
 					query += bid + ",";
